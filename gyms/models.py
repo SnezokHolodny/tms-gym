c7e9da8ee29d33abd80133c299c332f8d9204f80
Kolya Goroh <fasolka9520@gmail.com>
2023-08-26 19:49:36 +0300

    Initial commit

--- gyms/models.py
+++ gyms/models.py
@@ -36,37 +36,24 @@ class Abonement(models.Model):
 
 class PriceAbonement(models.Model):
     price = models.IntegerField(default=0)
-    abonement = models.ForeignKey(Abonement, on_delete=models.SET_NULL,
+    abonement = models.ForeignKey(Abonement, on_delete=models.CASCADE,
                                           null=True, blank=True, related_name='price')
 
 
 class NameAbonement(models.Model):
     name = models.CharField(max_length=100)
-    abonement = models.ForeignKey(Abonement, on_delete=models.SET_NULL,
+    abonement = models.ForeignKey(Abonement, on_delete=models.CASCADE,
                                           null=True, blank=True, related_name='name')
 
 
 class TimeAbonement(models.Model):
     time = models.CharField(max_length=100)
-    abonement = models.ForeignKey(Abonement, on_delete=models.SET_NULL,
+    abonement = models.ForeignKey(Abonement, on_delete=models.CASCADE,
                                           null=True, blank=True, related_name='time_abonement')
-
-
-class Order(models.Model):
-    profile = models.ForeignKey('Profile', on_delete=models.CASCADE,
-                                related_name='orders')
-    status = models.CharField(max_length=20, default="INITIAL")
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    shopping_cart = models.OneToOneField(Order, on_delete=models.SET_NULL,
-                                         null=True, blank=True, related_name='+')
 
 
 
-class OrderEntry(models.Model):
-    trainer = models.ForeignKey(Trainer, on_delete=models.CASCADE,
-                                related_name='+')
-    count = models.IntegerField(default=0)
-    order = models.ForeignKey('Order', on_delete=models.CASCADE, related_name='order_entries')
