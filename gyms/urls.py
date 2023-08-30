3e0299240a891a4edde77ce236b1d404289c986a
Kolya Goroh <fasolka9520@gmail.com>
2023-08-30 15:46:19 +0300

    Initial commit 3

--- gyms/urls.py
+++ gyms/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('trainer/<int:trainer_id>', views.trainer_detail, name='trainer_detail'),
     path('add_abonement/', views.add_abonement, name='add_abonement'),
     path('delete_abonement/', views.delete_abonement, name='delete_abonement'),
+    path('register/', views.register, name='register'),
+    path('personal_account/', views.information_of_user, name='information_of_user'),
+    path('edit_profile/<int:user_id>', views.edit_profile, name='edit_profile'),
 ]
--- gyms/views.py
+++ gyms/views.py
@@ -2,6 +2,13 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, HttpRequest
 from .models import Gym, Trainer, Abonement
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+from django.contrib import messages
+from .forms import UserForm, NewUserForm
+
+
+
 
 def index(request):
     return render(request, 'gyms/index.html', {'title': 'Тренажерки', 'gyms': Gym.objects.all()})
@@ -28,13 +35,50 @@ def add_abonement(request: HttpRequest):
     trainer = get_object_or_404(Trainer, id=request.POST['trainer_id'])
     profile = request.user.profile
     abonement = Abonement.objects.create(gym=gym, trainer=trainer, profile=profile, time=request.POST['time'])
-    return redirect('gyms:gym', request.POST['gym_id'])
+    return redirect('gyms:gym_detail', request.POST['gym_id'])
 
 def delete_abonement(request):
     gym = get_object_or_404(Gym, id=request.POST['gym_id'])
-    abonement = get_object_or_404(Abonement, gym__id=request.POST['gym_id'], profile=request.user.profile)
+    abonement = Abonement.objects.filter(gym=gym, profile=request.user.profile)
     abonement.delete()
     request.user.profile.save()
-    return redirect('gyms:gym', request.POST['gym_id'])
+    return redirect('gyms:gym_detail', request.POST['gym_id'])
+
+def register(request):
+    if request.method == 'POST':
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Вы зарегестрировались успешно.")
+            return redirect('gyms:index')
+    else:
+        messages.error(request, "Нормально регайся!")
+        form = NewUserForm()
+        return render(request, "registration/register.html", context={"form": form})
+
+@login_required
+def information_of_user(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+    return render(request, 'gyms/profile.html', {'user': user})
 
 
+@login_required
+def edit_profile(request, user_id):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            user = get_object_or_404(User, id=user_id)
+            user.username = username
+            user.email = email
+            user.first_name = first_name
+            user.last_name = last_name
+            user.save()
+            return redirect('gyms:profile', user.id)
+    else:
+        form = UserForm(instance=request.user)
+    return render(request, 'gyms/edit_profile.html', {'form': form})
